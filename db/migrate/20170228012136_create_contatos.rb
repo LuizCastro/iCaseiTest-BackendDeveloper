286f668e983aedf2df09ec893ee0b4552471fb7b
CastroLuiz <luizcastro@cursos-on.com>
2017-03-03 16:39:49 -0300

    Readme Update

--- db/migrate/20170228012136_create_contatos.rb
+++ db/migrate/20170228012136_create_contatos.rb
@@ -2,9 +2,10 @@ class CreateContatos < ActiveRecord::Migration[5.0]
   def up
     create_table :contatos do |t|
     	t.string "nome", :limit=>500,:null=> false 
-    	t.string "email", :limit=>100,:null=> false ,:unique => true
+    	t.string "email", :limit=>100,:null=> false
     	t.datetime "dt_incl", default:->{'CURRENT_TIMESTAMP'}
     	t.datetime "dt_edic"
+        t.index ["email"], name: "email_UNIQUE", unique: true, using: :btree
     end
     def down
     	drop_table :contatos
